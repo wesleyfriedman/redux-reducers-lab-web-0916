256db8b1cfe2eee99e5d28469e17bac2a05a4674
samnags <sam.nagourney@gmail.com>
2016-10-31 14:44:10 -0400

    empty master

--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-import ADD_INGREDIENT from './types'
-
-export function addIngredient(ingredient) {
-  return {
-    type: ADD_INGREDIENT,
-    payload: ingredient
-  }
-}
--- src/actions/types.js
+++
@@ -1,1 +0,0 @@
-export const ADD_INGREDIENT = 'ADD_INGREDIENT'
